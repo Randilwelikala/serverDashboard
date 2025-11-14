e7c8120c0aed979d5d3e0651c2fa17a70d9cbe50
randilwelikala <randilgimantha646@gmail.com>
2025-11-14 23:56:46 +0530

    create json file with dummy data

--- obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+506240ee7e72a067c6cedc1be8b7cd256ef7bda7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97cac4f45b3783ba957c8c99f4d3cb5b0163862c")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
