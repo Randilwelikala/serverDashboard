00f8f0c18397a0f9788115776d97da3798a67b3c
randilwelikala <randilgimantha646@gmail.com>
2025-11-27 00:29:35 +0530

    changed root class

--- Services/ServerServices.cs
+++ Services/ServerServices.cs
@@ -8,15 +8,13 @@ namespace serverDashboard.Services
         private readonly Root _root;
 
         public ServerService()
-        {
-            // Load JSON data
+        {           
             var json = System.IO.File.ReadAllText("wwwroot/data/clients.json");
             _root = JsonSerializer.Deserialize<Root>(json);
         }
 
-        public List<Client> GetClients()
-        {
-            return _root.Clients;
-        }
+        public List<Client> GetClients() => _root.Clients;
+        public Client GetClientById(string clientId) => _root.Clients.FirstOrDefault(c => c.ClientId == clientId);
+        public Server GetServer(string clientId, string serverId) => GetClientById(clientId)?.Servers.FirstOrDefault(s => s.ServerId == serverId);
     }
 }
