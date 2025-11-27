cf7e89202cfe1aafd198e21a20df901c3a1a83cb
randilwelikala <randilgimantha646@gmail.com>
2025-11-28 00:00:37 +0530

    fixed errors and now display client's name when clicking clients button in the bavbar

--- obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14b3b3206518d60bc918dce4001703fd5c752de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43047fd15afc34edc64a0d8a021bf4215f2c202e")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
