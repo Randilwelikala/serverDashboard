9057112196d822bb75c655cc69aaeb1c17766d43
randilwelikala <randilgimantha646@gmail.com>
2025-11-29 23:26:21 +0530

    display CPU usage in a dashboard

--- obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54eb05c25f1b04a4fc6a5e9d9b6643fa7cd1506c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654e09db629c63688dc62940b7a3bcde1d4ecb10")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
