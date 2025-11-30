7fef92b05a4465029c58edd93e12d6a81c2edead
randilwelikala <randilgimantha646@gmail.com>
2025-11-30 19:46:51 +0530

    changed

--- obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654e09db629c63688dc62940b7a3bcde1d4ecb10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9057112196d822bb75c655cc69aaeb1c17766d43")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
