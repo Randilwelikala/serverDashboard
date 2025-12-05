ec1c367b5b0dc4623f9526e40c3bb8cf5340e627
randilwelikala <randilgimantha646@gmail.com>
2025-12-05 22:31:26 +0530

    added new ids for div tags

--- obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
+++ obj/Debug/net8.0/serverDashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c262c251e930eba2e8588a87d7b62af8ec38225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f039a98ab99392365e2aedfd5aaddeec4f4a1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("serverDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
